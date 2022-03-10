4092d2fb6726db5972464357bc9bcee313b5b0d9
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-03-10 14:40:28 +0900

    init

---
+++ index.ts
@@ -0,0 +1,66 @@
+require('dotenv').config()
+import express, { Application, Request, Response } from "express";
+import cors from 'cors';
+import { KintoneRestAPIClient } from "@kintone/rest-api-client";
+
+const app: Application = express();
+const port = 3001;
+
+const client = new KintoneRestAPIClient({
+  baseUrl: "https://rdmuhwtt6gx7.cybozu.com",
+  // Use password authentication
+  /* auth: {
+    username: process.env.KINTONE_USERNAME,
+    password: process.env.KINTONE_PASSWORD,
+  }, */
+  // Use API token authentication
+  auth: { apiToken: process.env.KINTONE_API_TOKEN }
+  // Use OAuth token authentication
+  // auth: { oAuthToken: process.env.KINTONE_OAUTH_TOKEN }
+
+  // Use session authentication if `auth` is omitted (in browser only)
+});
+
+client.record.getAllRecords({
+  app: 183
+}).then(res => console.log('allRecords', res));
+
+console.log('KINTONE_API_TOKEN', process.env.KINTONE_API_TOKEN);
+
+
+client.record.addRecord({
+  app: 183,
+  record: {fullname: {value:'Hello'}}
+})
+
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors())
+
+app.get(
+    "/",
+    async (req: Request, res: Response): Promise<Response> => {
+        return res.status(200).send({
+            message: "Hello World!",
+        });
+    }
+);
+
+app.post(
+  "/cocoEnquete",
+  async (req: Request, res: Response): Promise<Response> => {
+
+      return res.status(200).send({
+          message: "Hello cocoEnquete World!" + JSON.stringify(req.body) ,
+      });
+  }
+);
+try {
+    app.listen(port, (): void => {
+        console.log(`Connected successfully on port ${port}`);
+    });
+} catch (error) {
+    console.error(`Error occured: `);
+}
